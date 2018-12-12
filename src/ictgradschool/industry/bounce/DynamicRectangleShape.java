1ff3533da4ecbbb2b9799d6681cec571e25fe32e
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-12-13 12:29:05 +1300

    adding Dynamic rect.

---
+++ src/ictgradschool/industry/bounce/DynamicRectangleShape.java
@@ -0,0 +1,65 @@
+package ictgradschool.industry.bounce;
+
+import javax.xml.bind.annotation.XmlElement;
+import java.awt.*;
+
+/**
+ * Class to represent a simple rectangle.
+ * 
+ * @author Ian Warren
+ */
+public class DynamicRectangleShape extends RectangleShape {
+//	adding default color of black and instance var to specify color at construction
+	private static final Color DEFAULT_COLOR = new Color(1,1,1);
+
+	protected Color color;
+	/**
+	 * Default constructor that creates a RectangleShape instance whose instance
+	 * variables are set to default values.
+	 */
+	public DynamicRectangleShape(Color color) {
+		fX=DEFAULT_X_POS;
+		fY=DEFAULT_Y_POS;
+		fDeltaX=DEFAULT_DELTA_X;
+		fDeltaY=DEFAULT_DELTA_Y;
+		fWidth=DEFAULT_WIDTH;
+		fHeight=DEFAULT_HEIGHT;
+		this.color=color;
+	}
+
+	/**
+	 * Creates a RectangleShape instance with specified values for instance
+	 * variables.
+	 * @param x x position.
+	 * @param y y position.
+	 * @param deltaX speed and direction for horizontal axis.
+	 * @param deltaY speed and direction for vertical axis.
+	 */
+	public DynamicRectangleShape(int x, int y, int deltaX, int deltaY) {
+		super(x,y,deltaX,deltaY);
+	}
+
+	/**
+	 * Creates a RectangleShape instance with specified values for instance
+	 * variables.
+	 * @param x x position.
+	 * @param y y position.
+	 * @param deltaX speed (pixels per move call) and direction for horizontal
+	 *        axis.
+	 * @param deltaY speed (pixels per move call) and direction for vertical
+	 *        axis.
+	 * @param width width in pixels.
+	 * @param height height in pixels.
+	 */
+	public DynamicRectangleShape(int x, int y, int deltaX, int deltaY, int width, int height) {
+		super(x,y,deltaX,deltaY,width,height);
+	}
+	
+	/**
+	 * Paints this RectangleShape object using the supplied Painter object.
+	 */
+	@Override
+	public void paint(Painter painter) {
+		painter.drawRect(fX,fY,fWidth,fHeight);
+	}
+}
